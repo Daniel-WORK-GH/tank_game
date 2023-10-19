b6f316989563bc27bd80110276b891df819f680a
DanielGH3 <danielnazuni@gmail.com>
2023-10-19 07:04:39 +0300

    Update publish.py code

--- publish.py
+++ publish.py
@@ -1,5 +1,12 @@
 import subprocess
 
-subprocess.call(["git", "add", "*"])
-subprocess.call(["git", "commit", "--all", f"""-m"{input("Commit messege : ")}" """])
-subprocess.call(["git", "push"])
+gitadd = "git add *"
+gitcommit = f"""git commit --all -m"{input("Commit message: ")}" """
+gitpush = "git push"
+
+def to_args(str:str):
+    return str.strip().split(' ')
+
+subprocess.call(to_args(gitadd))
+subprocess.call(to_args(gitcommit))
+subprocess.call(to_args(gitpush))
