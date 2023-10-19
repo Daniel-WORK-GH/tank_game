ed043c6790f594d2c88711a8ccb5706258f43b0d
DanielGH3 <danielnazuni@gmail.com>
2023-10-19 07:08:03 +0300

    Update publishing code

--- publish.py
+++ publish.py
@@ -7,6 +7,6 @@ gitpush = "git push"
 def to_args(str:str):
     return str.strip().split(' ')
 
-subprocess.call(to_args(gitadd))
-subprocess.call(to_args(gitcommit))
-subprocess.call(to_args(gitpush))
+subprocess.call(gitadd)
+subprocess.call(gitcommit)
+subprocess.call(gitpush)
